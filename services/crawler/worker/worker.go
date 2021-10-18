9feb9a0ced040687e4fdd8c2878558d129be0caf
IamCathal <cathalsocallaghan@gmail.com>
2021-10-18 17:58:46 +0100

    Crawler: Add basic steam web API structs and worker funcs

---
+++ services/crawler/worker/worker.go
@@ -0,0 +1,17 @@
+package worker
+
+// Worker is a worker pool function that processes jobs asynchronously
+func Worker() {
+
+}
+
+// GetFriends gets the friendslist for a given user through either the steam web API
+// or cache
+func GetFriends() {
+
+}
+
+// ControlFunc manages workers
+func ControlFunc() {
+
+}

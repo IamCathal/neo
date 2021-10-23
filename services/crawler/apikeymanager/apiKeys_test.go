5c53c6dddda493cc42063ece76ca34b5883be5b3
IamCathal <cathalsocallaghan@gmail.com>
2021-10-23 15:54:36 +0100

    Crawler: Add first apikeymanager service tests

---
+++ services/crawler/apikeymanager/apiKeys_test.go
@@ -0,0 +1,42 @@
+package apikeymanager
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/iamcathal/neo/services/crawler/configuration"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestThatValidAPIKeysCanBeExtractedFromEnvFile(t *testing.T) {
+	os.Setenv("STEAM_API_KEYS", "Quick,Brown,Fox,Ran")
+
+	InitApiKeys()
+	expectedAPIKeys := []string{"Quick", "Brown", "Fox", "Ran"}
+	actualAPIKeys := []string{}
+	for _, APIKey := range configuration.UsableAPIKeys.APIKeys {
+		actualAPIKeys = append(actualAPIKeys, APIKey.Key)
+	}
+
+	assert.Equal(t, actualAPIKeys, expectedAPIKeys)
+}
+
+func TestGetSteamAPIKeyThrottlesRequests(t *testing.T) {
+	keySleepTime := 15
+	os.Setenv("STEAM_API_KEYS", "Quick,Brown,Fox,Ran")
+	os.Setenv("KEY_SLEEP_TIME", fmt.Sprint(keySleepTime))
+	InitApiKeys()
+
+	startTime := time.Now()
+	_ = GetSteamAPIKey()
+	timeTaken := time.Since(startTime) * time.Millisecond
+
+	assert.GreaterOrEqual(t, timeTaken, time.Duration(keySleepTime)*time.Millisecond)
+}

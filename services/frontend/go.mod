e019419159a014a51df12bd0cc3d651ecd6b0abe
IamCathal <cathalsocallaghan@gmail.com>
2021-10-23 18:50:39 +0100

    Frontend: Add working stats monitoring

--- services/frontend/go.mod
+++ services/frontend/go.mod
@@ -4,8 +4,9 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/influxdata/influxdb-client-go v1.4.0 // indirect
+	github.com/influxdata/influxdb-client-go v1.4.0
 	github.com/joho/godotenv v1.4.0
+	github.com/mackerelio/go-osstat v0.2.1 // indirect
 	github.com/segmentio/ksuid v1.0.4
 	github.com/stretchr/testify v1.7.0
 	go.uber.org/zap v1.19.1
--- services/frontend/statsmonitoring/statsMonitoring.go
+++ services/frontend/statsmonitoring/statsMonitoring.go
@@ -6,73 +6,49 @@ import (
 	"log"
 	"math"
 	"os"
-	"os/exec"
-	"strconv"
-	"strings"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go"
+	"github.com/mackerelio/go-osstat/cpu"
+	"github.com/mackerelio/go-osstat/memory"
 )
 
 func getCPUUsagePercentage(cpuUsage *float64) {
 	for {
-		out, err := exec.Command("bash", "-c", "awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) ; }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat)").CombinedOutput()
+		before, err := cpu.Get()
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		cpuUsageFloat, err := strconv.ParseFloat(strings.ReplaceAll(string(out), "\n", ""), 64)
+		time.Sleep(time.Duration(1) * time.Second)
+		after, err := cpu.Get()
 		if err != nil {
-			fmt.Printf("Err: %+v\n", err)
-			*cpuUsage = 0
+			log.Fatal(err)
 		}
-		*cpuUsage = cpuUsageFloat
-		time.Sleep(3 * time.Second)
+		total := float64(after.Total - before.Total)
+
+		*cpuUsage = float64(after.Idle-before.Idle) / total * 100
+		time.Sleep(8 * time.Second)
 	}
 }
 
 func getTotalRAM(totalRAM *float64) {
-	out, err := exec.Command("bash", "-c", "free -t | awk 'NR == 2 {printf(\"%.2f\", $2/1000000)}'").CombinedOutput()
+	memory, err := memory.Get()
 	if err != nil {
 		log.Fatal(err)
 	}
-	totalRAMFloat, err := strconv.ParseFloat(strings.ReplaceAll(string(out), "\n", ""), 64)
-	if err != nil {
-		fmt.Printf("Err: %+v\n", err)
-		*totalRAM = 0
-	}
-	*totalRAM = totalRAMFloat
+	memTotalMB := float64(((memory.Total / 1000) / 1000))
+	*totalRAM = memTotalMB
 }
 
-func getRamUsedCurrently(RAMUsedCurrently *float64) {
+func GetCurrentRAMUsage(ramUsedNow *float64) {
 	for {
-		out, err := exec.Command("bash", "-c", "free -t | awk 'NR == 2 {printf(\"%.2f\", $3/1000000)}'").CombinedOutput()
+		memory, err := memory.Get()
 		if err != nil {
 			log.Fatal(err)
 		}
-		RAMUsedCurrentlyFloat, err := strconv.ParseFloat(strings.ReplaceAll(string(out), "\n", ""), 64)
-		if err != nil {
-			fmt.Printf("Err: %+v\n", err)
-			*RAMUsedCurrently = 0
-		}
-		*RAMUsedCurrently = RAMUsedCurrentlyFloat
-		time.Sleep(3 * time.Second)
-	}
-}
-
-func getTotalDiskSpaceUsed(diskSpaceUsed *float64) {
-	for {
-		out, err := exec.Command("bash", "-c", "df --output=size --total | awk 'END {print $1}'").CombinedOutput()
-		if err != nil {
-			fmt.Println(err)
-		}
-		diskSpaceUsedCurrently, err := strconv.ParseFloat(strings.ReplaceAll(string(out), "\n", ""), 64)
-		if err != nil {
-			// fmt.Println(err)
-			*diskSpaceUsed = 0
-		}
-		*diskSpaceUsed = diskSpaceUsedCurrently / 1000000
-		time.Sleep(3 * time.Second)
+		memUsedMB := float64(((memory.Used / 1000) / 1000))
+		*ramUsedNow = memUsedMB
+		time.Sleep(8 * time.Second)
 	}
 }
 
@@ -86,7 +62,7 @@ func CollectAndShipStats() {
 	getTotalRAM(&totalRAM)
 	// Get live CPU and RAM usage stats on a regular interval
 	go getCPUUsagePercentage(&cpuUsagePercentage)
-	go getRamUsedCurrently(&RAMUsedCurrently)
+	go GetCurrentRAMUsage(&RAMUsedCurrently)
 
 	client := influxdb2.NewClient(os.Getenv("INFLUXDB_URL"), os.Getenv("SYSTEM_STATS_BUCKET_TOKEN"))
 	defer client.Close()
@@ -96,19 +72,19 @@ func CollectAndShipStats() {
 
 	for {
 		// fmt.Printf("Current cpu usage is: %v%% - Total RAM: %v Gb - Used now: %v Gb\n", cpuUsagePercentage, totalRAM, RAMUsedCurrently)
-		time.Sleep(5 * time.Second)
 		point := influxdb2.NewPointWithMeasurement("systemStatusCPU").
 			AddTag("system", os.Getenv("NODE_NAME")).
 			AddField("cpu", cpuUsagePercentage).
 			SetTime(time.Now())
 		writeAPI.WritePoint(context.Background(), point)
 
+		memUsagePercentage := float64(RAMUsedCurrently) / float64(totalRAM)
+
 		point = influxdb2.NewPointWithMeasurement("systemStatusRAM").
-			AddTag("system", fmt.Sprintf("%s (%.0f GB)", os.Getenv("NODE_NAME"), math.Floor(totalRAM))).
-			AddField("memory", math.Floor((RAMUsedCurrently/totalRAM)*100)).
+			AddTag("system", fmt.Sprintf("%s (%.0f GB)", os.Getenv("NODE_NAME"), totalRAM/1000)).
+			AddField("memory", math.Floor(memUsagePercentage*100)).
 			SetTime(time.Now())
 		writeAPI.WritePoint(context.Background(), point)
-
-		time.Sleep(time.Second * 1)
+		time.Sleep(10 * time.Second)
 	}
 }

dc17a6cb829b3df5e06e5ee9466f2681651c1f10
IamCathal <cathalsocallaghan@gmail.com>
2022-01-17 17:14:49 +0000

    Frontend: Add retrieval of processed graph data

---
+++ services/frontend/static/javascript/graphPage.js
@@ -0,0 +1,61 @@
+function getProcessedGraphData(crawlID) {
+    return new Promise((resolve, reject) => {
+        fetch(`http://localhost:2590/api/getprocessedgraphdata/${crawlID}`, {
+            method: "POST",
+        }).then(res => res.json())
+        .then(data => {
+            resolve(data)
+        }).catch(err => {
+            reject(err)
+        })
+    });
+}
+
+// COMMON
+function setUserCardDetailZ(userObj) {
+    console.log(userObj)
+    document.getElementById("userUsername").textContent = userObj.User.accdetails.personaname;
+    document.getElementById("userRealName").textContent = "idk";
+    document.getElementById("userFriendCount").textContent = userObj.User.friendids.length;
+    
+    const creationDate = new Date(userObj.User.accdetails.timecreated*1000);
+    const dateString = `${creationDate.getDate()} ${creationDate.toLocaleString('default', { month: 'long' })} ${creationDate.getFullYear()}`;
+    const timeSinceString = `(${timezSince2(creationDate)} ago)`
+    document.getElementById("userCreationDate").textContent = `${dateString} ${timeSinceString}`;
+    
+    document.getElementById("userSteamID").textContent = userObj.User.accdetails.steamid;
+    document.getElementById("userAvatar").src = userObj.User.accdetails.avatar.split(".jpg").join("") + "_full.jpg";
+
+    document.getElementById("userUsername").classList.remove("skeleton");
+    document.getElementById("userRealName").classList.remove("skeleton");
+    document.getElementById("userFriendCount").classList.remove("skeleton");
+    document.getElementById("userCreationDate").classList.remove("skeleton");
+    document.getElementById("userSteamID").classList.remove("skeleton");
+    document.getElementById("userAvatar").classList.remove("skeleton");
+}
+
+// COMMON
+function timezSince2(targetDate) {
+    let seconds = Math.floor((new Date()-targetDate)/1000)
+    let interval = seconds / 31536000 
+    if (interval > 1) {
+        return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000; // months
+    if (interval > 1) {
+        return Math.floor(interval) + " months";
+      }
+    interval = seconds / 86400; // days
+    if (interval > 1) {
+      return Math.floor(interval) + "d ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + "h ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + "m ago";
+    }
+    return Math.floor(seconds) + "s";
+}
---
+++ services/frontend/static/javascript/userCard.js
@@ -0,0 +1,21 @@
+function setUserCardDetails(userObj) {
+    console.log(userObj)
+    document.getElementById("userUsername").textContent = userObj.User.accdetails.personaname;
+    document.getElementById("userRealName").textContent = "idk";
+    document.getElementById("userFriendCount").textContent = userObj.user.friendids.length;
+    
+    const creationDate = new Date(userObj.user.accdetails.timecreated*1000);
+    const dateString = `${creationDate.getDate()} ${creationDate.toLocaleString('default', { month: 'long' })} ${creationDate.getFullYear()}`;
+    const timeSinceString = `(${timezSince(creationDate)} ago)`
+    document.getElementById("userCreationDate").textContent = `${dateString} ${timeSinceString}`;
+    
+    document.getElementById("userSteamID").textContent = userObj.user.accdetails.steamid;
+    document.getElementById("userAvatar").src = userObj.user.accdetails.avatar.split(".jpg").join("") + "_full.jpg";
+
+    document.getElementById("userUsername").classList.remove("skeleton");
+    document.getElementById("userRealName").classList.remove("skeleton");
+    document.getElementById("userFriendCount").classList.remove("skeleton");
+    document.getElementById("userCreationDate").classList.remove("skeleton");
+    document.getElementById("userSteamID").classList.remove("skeleton");
+    document.getElementById("userAvatar").classList.remove("skeleton");
+}

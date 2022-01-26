f8c45443d5eb35fe1e4a4633938b26dab56f6d20
IamCathal <cathalsocallaghan@gmail.com>
2022-01-26 21:55:47 +0000

    Frontend: Add example code for game bar chart

--- services/frontend/static/javascript/graphPage.js
+++ services/frontend/static/javascript/graphPage.js
@@ -30,6 +30,7 @@ doesProcessedGraphDataExistz(crawlID).then(doesExist => {
         fillInTop10Countries(countryFrequencies)
         fillInFromYourCountryStatBox(crawlDataObj, countryFrequencies)
         fillInContinentCoverage(countryFrequencies)
+        initAndRenderGamesBarChart()
 
         var myChart = echarts.init(document.getElementById('graphContainer'));
         const graph = getDataInGraphFormat(crawlDataObj.usergraphdata)
@@ -84,8 +85,8 @@ doesProcessedGraphDataExistz(crawlID).then(doesExist => {
                 },
                 force: {
                     gravity: 0.5,
-                    repulsion: 270,
-                    friction: 0.2
+                    repulsion: 370,
+                    friction: 0.2,
                 }
             }
             ]
@@ -116,7 +117,7 @@ function getProcessedGraphData(crawlID) {
 function getDataInGraphFormat(gData) {
     let nodes = []
     let links = []
-
+    console.log("push main user")
     nodes.push({
         "id": gData.userdetails.User.accdetails.steamid,
         "name": gData.userdetails.User.accdetails.personaname,
@@ -134,14 +135,16 @@ function getDataInGraphFormat(gData) {
         });
     });
 
-    gData.userdetails.User.friendids.forEach(ID => {
-        links.push({"source":gData.userdetails.User.accdetails.steamid, "target": `${ID}`})
-    })
-    gData.frienddetails.forEach(friend => {
-        friend.User.friendids.forEach(friendID => {
-            links.push({"source":friend.User.accdetails.steamid, "target": `${friendID}`})
+    if (gData.userdetails.User.friendids.length <= 500) {
+        gData.userdetails.User.friendids.forEach(ID => {
+            links.push({"source":gData.userdetails.User.accdetails.steamid, "target": `${ID}`})
         })
-    })
+        gData.frienddetails.forEach(friend => {
+            friend.User.friendids.forEach(friendID => {
+                links.push({"source":friend.User.accdetails.steamid, "target": `${friendID}`})
+            })
+        })
+    }
 
     const echartsData = {
         "nodes": nodes,
@@ -149,6 +152,175 @@ function getDataInGraphFormat(gData) {
         "categories": [{"name": "A"}]
     }
     return echartsData
+}
+
+function initAndRenderGamesBarChart() {
+    console.log("renduing")
+    let app = {};
+
+    let chartDom = document.getElementById('gamesBarChartContainer');
+    // var myChart = echarts.init(chartDom, 'dark');
+    let myChart = echarts.init(chartDom);
+    let option;
+
+    const posList = [
+        'left',
+        'right',
+        'top',
+        'bottom',
+        'inside',
+        'insideTop',
+        'insideLeft',
+        'insideRight',
+        'insideBottom',
+        'insideTopLeft',
+        'insideTopRight',
+        'insideBottomLeft',
+        'insideBottomRight'
+    ];
+    app.configParameters = {
+    rotate: {
+        min: -90,
+        max: 90
+    },
+    align: {
+        options: {
+            left: 'left',
+            center: 'center',
+            right: 'right'
+        }
+    },
+    verticalAlign: {
+        options: {
+            top: 'top',
+            middle: 'middle',
+            bottom: 'bottom'
+        }
+    },
+    position: {
+        options: posList.reduce(function (map, pos) {
+            map[pos] = pos;
+            return map;
+        }, {})
+    },
+    distance: {
+        min: 0,
+        max: 100
+    }
+    };
+    app.config = {
+        rotate: 90,
+        align: 'left',
+        verticalAlign: 'middle',
+        position: 'insideBottom',
+        distance: 15,
+        onChange: function () {
+            const labelOption = {
+            rotate: app.config.rotate,
+            align: app.config.align,
+            verticalAlign: app.config.verticalAlign,
+            position: app.config.position,
+            distance: app.config.distance
+            };
+            myChart.setOption({
+            series: [
+                { label: labelOption },
+                { label: labelOption },
+                { label: labelOption },
+                { label: labelOption }
+            ]
+            });
+        }
+    };
+    const labelOption = {
+    show: true,
+    position: app.config.position,
+    distance: app.config.distance,
+    align: app.config.align,
+    verticalAlign: app.config.verticalAlign,
+    rotate: app.config.rotate,
+    formatter: '{c}  {name|{a}}',
+    fontSize: 16,
+    rich: {
+        name: {}
+    }
+    };
+    option = {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+        type: 'shadow'
+        }
+    },
+    legend: {
+        data: ['Forest', 'Steppe', 'Desert', 'Wetland']
+    },
+    toolbox: {
+        show: true,
+        orient: 'vertical',
+        left: 'right',
+        top: 'center',
+        feature: {
+        mark: { show: true },
+        dataView: { show: true, readOnly: false },
+        magicType: { show: true, type: ['line', 'bar', 'stack'] },
+        restore: { show: true },
+        saveAsImage: { show: true }
+        }
+    },
+    xAxis: [
+        {
+        type: 'category',
+        axisTick: { show: false },
+        data: ['2012', '2013', '2014', '2015', '2016']
+        }
+    ],
+    yAxis: [
+        {
+        type: 'value'
+        }
+    ],
+    series: [
+        {
+        name: 'Forest',
+        type: 'bar',
+        barGap: 0,
+        label: labelOption,
+        emphasis: {
+            focus: 'series'
+        },
+        data: [320, 332, 301, 334, 390]
+        },
+        {
+        name: 'Steppe',
+        type: 'bar',
+        label: labelOption,
+        emphasis: {
+            focus: 'series'
+        },
+        data: [220, 182, 191, 234, 290]
+        },
+        {
+        name: 'Desert',
+        type: 'bar',
+        label: labelOption,
+        emphasis: {
+            focus: 'series'
+        },
+        data: [150, 232, 201, 154, 190]
+        },
+        {
+        name: 'Wetland',
+        type: 'bar',
+        label: labelOption,
+        emphasis: {
+            focus: 'series'
+        },
+        data: [98, 77, 101, 99, 40]
+        }
+    ]
+    };
+    option && myChart.setOption(option);
 }
 
 // COMMON

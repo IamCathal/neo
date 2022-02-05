704603b6a746ac1600db82e9eb20712caf37a121
IamCathal <cathalsocallaghan@gmail.com>
2022-02-05 21:25:55 +0000

    Frontend: Make threeJS graph fullscreen

--- services/frontend/static/javascript/graphPage.js
+++ services/frontend/static/javascript/graphPage.js
@@ -46,6 +46,8 @@ doesProcessedGraphDataExistz(crawlID).then(doesExist => {
         // Three JS bottom test graph
         initThreeJSGraph(crawlDataObj.usergraphdata)
 
+        initGamerScore()
+
         var myChart = echarts.init(document.getElementById('graphContainer'));
         const graph = getDataInGraphFormat(crawlDataObj.usergraphdata)
         var option;
@@ -1036,6 +1038,98 @@ function fillInContinentCoverage(countryFreqs) {
     document.getElementById("statBoxContinentCoverage").textContent = Math.floor(continentCoverage*100)+"%";
     removeSkeletonClasses(["statBoxContinentCoverage"])
     return
+}
+
+function initGamerScore() {
+    let chartDom = document.getElementById('gamerScore');
+    let myChart = echarts.init(chartDom);
+    let option;
+
+    option = {
+    series: [
+        {
+        type: 'gauge',
+        startAngle: 180,
+        endAngle: 0,
+        min: 0,
+        max: 1,
+        splitNumber: 8,
+        axisLine: {
+            lineStyle: {
+            width: 8,
+            color: [
+                [0.25, '#FF6E76'],
+                [0.5, '#FDDD60'],
+                [0.75, '#58D9F9'],
+                [1, '#7CFFB2']
+            ]
+            }
+        },
+        pointer: {
+            icon: 'path://M12.8,0.7l12,40.1H0.7L12.8,0.7z',
+            length: '12%',
+            width: 20,
+            offsetCenter: [0, '-60%'],
+            itemStyle: {
+            color: 'auto'
+            }
+        },
+        axisTick: {
+            length: 12,
+            lineStyle: {
+            color: 'auto',
+            width: 2
+            }
+        },
+        splitLine: {
+            length: 20,
+            lineStyle: {
+            color: 'auto',
+            width: 5
+            }
+        },
+        axisLabel: {
+            color: '#ffffff',
+            fontSize: 30,
+            distance: -60,
+            formatter: function (value) {
+            if (value === 0.875) {
+                return 'A';
+            } else if (value === 0.625) {
+                return 'B';
+            } else if (value === 0.375) {
+                return 'C';
+            } else if (value === 0.125) {
+                return 'D';
+            }
+            return '';
+            }
+        },
+        title: {
+            offsetCenter: [0, '-20%'],
+            fontSize: 110
+        },
+        detail: {
+            fontSize: 90,
+            textStyle: {
+                color: '#ffffff'
+            },
+            offsetCenter: [0, '0%'],
+            valueAnimation: true,
+            formatter: function (value) {
+            return Math.round(value * 100);
+            },
+            color: 'auto'
+        },
+        data: [
+            {
+            value: 0.7,
+            }
+        ]
+        }
+    ]
+    };
+    option && myChart.setOption(option);
 }
 
 function removeSkeletonClasses(elementIDs) {

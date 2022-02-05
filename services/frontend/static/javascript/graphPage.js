ea541a736ce096494b401f1cf9c81346536aad58
IamCathal <cathalsocallaghan@gmail.com>
2022-02-05 23:53:52 +0000

    Frontend: Add tutorial total hours barchart code

--- services/frontend/static/javascript/graphPage.js
+++ services/frontend/static/javascript/graphPage.js
@@ -879,50 +879,58 @@ function fillInFlagsDiv(friends) {
 }
 
 function initNetWorkMostHoursPlayedBarChart(data) {
-    // let chartDom = document.getElementById('networkMostHoursPlayedBarChart');
-    // let myChart = echarts.init(chartDom);
-    // let option;
+    console.log("yipeee")
+    let chartDom = document.getElementById('networkMostHoursPlayedBarChart');
+    let myChart = echarts.init(chartDom);
+    let option;
 
-    // option = {
-    // title: {
-    //     text: 'World Population'
-    // },
-    // tooltip: {
-    //     trigger: 'axis',
-    //     axisPointer: {
-    //     type: 'shadow'
-    //     }
-    // },
-    // legend: {},
-    // grid: {
-    //     left: '3%',
-    //     right: '4%',
-    //     bottom: '3%',
-    //     containLabel: true
-    // },
-    // xAxis: {
-    //     type: 'value',
-    //     boundaryGap: [0, 0.01]
-    // },
-    // yAxis: {
-    //     type: 'category',
-    //     data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World']
-    // },
-    // series: [
-    //     {
-    //     name: '2011',
-    //     type: 'bar',
-    //     data: [18203, 23489, 29034, 104970, 131744, 630230]
-    //     },
-    //     {
-    //     name: '2012',
-    //     type: 'bar',
-    //     data: [19325, 23438, 31000, 121594, 134141, 681807]
-    //     }
-    // ]
-    // };
+    option = {
+    title: {
+        text: 'World Population',
+        textStyle: {
+            color: '#ffffff'
+        }
+    },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    legend: {},
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: {
+        type: 'value',
+        boundaryGap: [0, 0.01],
+        axisLine: {
+            lineStyle: {
+                color: '#ffffff'
+            }
+        }
+    },
+    yAxis: {
+        type: 'category',
+        data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World'],
+        axisLine: {
+            lineStyle: {
+                color: '#ffffff'
+            }
+        }
+    },
+    series: [
+        {
+        type: 'bar',
+        data: [18203, 23489, 29034, 104970, 131744, 630230]
+        }
+    ]
+    };
 
-    // option && myChart.setOption(option);
+    option && myChart.setOption(option);
 }
 
 function fillInTopStatBoxes(graphData, countryFreqs) {
